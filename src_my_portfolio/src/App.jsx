cd81b5969aa7973403b90e4cc8117f40e3f59e14
mvtvn78 <mvtvn78@gmail.com>
2024-08-29 20:00:22 +0700

    raw layout

--- src_my_portfolio/src/App.jsx
+++ src_my_portfolio/src/App.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import { Card, Layout, Tooltip } from "antd";
+import React, { useEffect, useState } from 'react';
+import { Avatar, Card, Flex, Layout, Switch, Tooltip } from "antd";
 import video from './data/5636516356524.mp4';
+import avatar from './data/34832482.jpg'
 import "./css/cover_photo.css";
+import "./css/avatar.css";
 import {MutedOutlined,SoundOutlined,GoogleOutlined,GithubOutlined,WhatsAppOutlined } from '@ant-design/icons';
 import { Button } from "antd";
+import data from './data/data';
+import Meta from 'antd/es/card/Meta';
 const { Header, Footer, Sider, Content } = Layout;
-
 const headerStyle = {
   textAlign: 'center',
-  color: '#fff',
   lineHeight: '64px',
   padding:0,
-  height:'300px',
-  backgroundColor: '#FFFF1F',
+  height:'400px',
+  backgroundColor: '#FFFFFF',
+  position: "relative"
 };
 const contentStyle = {
   textAlign: 'center',
@@ -26,15 +29,18 @@ const layoutStyle = {
   overflow: 'hidden',
   width: '100%',
   padding: '0 120px',
-  height: "500px",
+  height: "100% ",
   backgroundColor: '#FFFFFF',
 };
 const App = () => {
   const [isMuted,SetIsMuted] = useState(true)
+  useEffect(()=>{
+    console.log(data);
+  },[])
   return ( 
       <Card>
         <Layout style={layoutStyle}>
-            <Header style={headerStyle} >
+            <Header  style={headerStyle}  >
             <div className="cover_photo">
               <div className="p_btn_on_cover_photo">
                 <Tooltip title="Bật âm thanh" >
@@ -51,17 +57,59 @@ const App = () => {
                   </div>
               </div>
                 <video muted={isMuted}  autoPlay loop id='video'>
-                  
                     <source src={video} type="video/mp4" />
-                    
                 </video>
             </div>
-
+            <div className="avatar">
+                    <img src={avatar} alt="" />
+              </div>
+            <div className="my_name">
+                  <span>Mai Văn Tiền(Mvt)</span>
+            </div>
             </Header>
-            {/* <Content style={contentStyle}>
+            <Content style={contentStyle}>
+              <Flex gap="small" wrap justify='center' >
+                <Button type="primary" block size="large">Sản phẩm</Button>
+                <Button  block size="large">Video của tôi</Button>
+              </Flex>
 
+              <div style={ {marginTop: '20px'}} >
+              <Card title="Các sản phẩm của tôi" bordered style={{textAlign:'left'}}  size ='default'>
+                <Flex wrap gap='25px' >
+                  {data.map((val,idx)=>{
+                    return (
+                      <div className="product-item" key={idx}>
+                        <Card
+                            bordered={false}
+                            hoverable
+                            cover={<img alt="example" src={val.image} 
+                             />}
+                            size ='default'
+                            style={
+                              {height:'100%'}
+                            }
+                          >         
+                            <Meta  avatar={
+                            <Avatar src={val.avatar} />} 
+                            title={val.title}
+                            description={
+                              <>
+                                <h4>{val.language}</h4>
+                                <p>{val.linkGithub}</p>
+                                <p style={{textAlign:'right'}}><b>{val.finishedDay}</b></p>
+                              </>
+                            } />
+                          </Card>
+                    </div>
+                    )
+                  })}
+                  
+                </Flex>
+                </Card>
+
+              </div>
             </Content>
-            */}
+           
         </Layout>
     </Card>
   )
---
+++ src_my_portfolio/src/data/data.jsx
@@ -0,0 +1,76 @@
+export default [
+
+    {
+        "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYrNVwxlBhUzwBd6H4YqdJckckasr4N6PjQw&s",
+        "title": "Backend Âm nhạc Mt5",
+        "language":"Node JS / MySQL /MVC",
+        "image": require("./demoBEmt5.png"),
+        "linkGithub":"github.com/mvtvn78/BackEnd_mt5",
+        "finishedDay": "Jun 11, 2024"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "QLSV / SQL Server",
+        "language":"Windows Forms : C#",
+        "image": require("./student_manager.png"),
+        "linkGithub":"github.com/mvtvn78/Final_HomeWork_Win",
+        "finishedDay": "Jun 9, 2024"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Trang Âm nhạc Mt5",
+        "language":"React JS / Bootstrap",
+        "image": require("./mt5_user.png"),
+        "linkGithub":"github.com/mvtvn78/Front_end_mt5",
+        "finishedDay": "May 22, 2024"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Trang Admin Âm nhạc Mt5",
+        "language":"Jquery /Ajax/ HTML /CSS / JS",
+        "image": require("./admin_mt5.png"),
+        "linkGithub":"github.com/mvtvn78/TeamPTHH",
+        "finishedDay": "Apr 22, 2024"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Website Game Store",
+        "language":"ReactJS framework",
+        "image": require("./webshop.png"),
+        "linkGithub":"github.com/mvtvn78/MvtGameShop",
+        "finishedDay": "Mar 27, 2024"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Game Trí Tuệ Sắp Xếp",
+        "language":"Python / Pygame",
+        "image": require("./puzzle_game.png"),
+        "linkGithub":"github.com/mvtvn78/PuzzleGame",
+        "finishedDay": "Dec 22, 2023"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Làm quen với game",
+        "language":"C++ / SDL2+ ",
+        "image": require("./MotoA1.png"),
+        "linkGithub":"github.com/mvtvn78/GameSimple",
+        "finishedDay": "Oct 11, 2023"
+    }, 
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Luyện thi Mô Tô A1",
+        "language":"Windows Forms : C#",
+        "image": require("./MotoA1.png"),
+        "linkGithub":"github.com/mvtvn78/MotoA1",
+        "finishedDay": "Aug 12, 2023"
+    },
+    {
+        "avatar": "https://s3-ap-southeast-1.amazonaws.com/homepage-media/wp-content/uploads/2020/03/04092434/NET_Core_Logo.png",
+        "title": "Quản lý Sinh Viên",
+        "language":"Windows Forms : C#",
+       "image": require("./student_manage.png"),
+        "linkGithub":"github.com/mvtvn78/StudentManagement_v1.0",
+        "finishedDay": "Jul 21, 2023"
+    }
+
+]
