37fd1367b0051053b048f5e2fd524155477c54a7
mvtvn78 <mvtvn78@gmail.com>
2025-04-02 19:39:11 +0700

    uupdate

--- src_my_portfolio/src/services/info/index.jsx
+++ src_my_portfolio/src/services/info/index.jsx
@@ -1,6 +1,6 @@
 export default {
     authorName:"Mai Văn Tiền (Mvt)",
-    avatarURL : require("../../data/avt2025.jpg"),
+    avatarURL : require("../../data/Tvm_new.jpg"),
     coverVideo : require("../../data/5636516356524.mp4"),
     githubLink: 'https://github.com/mvtvn78',
     emailContact : 'mvt78vn@gmail.com',
